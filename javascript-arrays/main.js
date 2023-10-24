865f90a5af9f2841377c5bd575131121c1192168
Henry Pham <hqpham98@gmail.com>
2023-10-24 17:42:03 +0000

    Removed for in loop

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -1,6 +1,6 @@
 const colors = ['red', 'white', 'blue'];
 
-for (const i in colors) {
+for (let i = 0; i < 3; i++) {
   console.log('value of colors[' + i + ']:', colors[i]);
 }
 
@@ -15,7 +15,7 @@ console.log('value of colors:', colors);
 
 const students = ['Ken', 'Barbie', 'Allan', 'Bob'];
 const numberOfStudents = students.length;
-console.log('value of numberOfStudents:', numberOfStudents);
+console.log('value of numberOfStudents:,', numberOfStudents);
 
 const lastIndex = numberOfStudents - 1;
 const lastStudent = students[lastIndex];
