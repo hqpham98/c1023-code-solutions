ac1fe842d2d79fbd1913a2e845e3297a31049939
Henry Pham <hqpham98@gmail.com>
2023-11-07 20:45:21 +0000

    completed exercise

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -2,15 +2,18 @@ const $openButton = document.querySelector('.open-button');
 const $modal = document.querySelector('.modal');
 const $overlay = document.querySelector('.overlay');
 const $noButton = document.querySelector('.no-button');
+const $container = document.querySelector('.container');
 let modalOpened = false;
 
 function handleOpenClick(event) {
   if (!modalOpened) {
     $modal.classList.remove('hidden');
     $overlay.classList.remove('hidden');
+    $container.classList.remove('hidden');
   } else {
     $modal.classList.add('hidden');
     $overlay.classList.add('hidden');
+    $container.classList.add('hidden');
   }
   modalOpened = !modalOpened;
 }
@@ -19,9 +22,11 @@ function handleNoClick(event) {
   if (modalOpened) {
     $modal.classList.add('hidden');
     $overlay.classList.add('hidden');
+    $container.classList.add('hidden');
   } else {
     $modal.classList.remove('hidden');
     $overlay.classList.remove('hidden');
+    $container.classList.remove('hidden');
   }
   modalOpened = !modalOpened;
 }
