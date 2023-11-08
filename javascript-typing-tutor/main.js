0d8cacb29e4bf91c6362c824cb21ce231750b242
Henry Pham <hqpham98@gmail.com>
2023-11-08 18:59:20 +0000

    implemented corrections

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,21 +1,21 @@
-const charArray = Array.from(document.querySelectorAll('span'));
-const $html = document.querySelector('html');
+const charArray = document.querySelectorAll('span');
 let i = 0;
 
 function handleKeyPress(event) {
+  console.dir(event);
   if (i < charArray.length) {
-    if (event.key.trim() === charArray[i].textContent.trim()) {
+    if (event.key === charArray[i].textContent) {
       charArray[i].classList.add('green');
+      charArray[i].classList.remove('red');
+      charArray[i].classList.remove('current-char');
+      i++;
+      if (i < charArray.length) {
+        charArray[i].classList.add('current-char');
+      }
     } else {
       charArray[i].classList.add('red');
-    }
-    charArray[i].classList.remove('current-char');
-    i++;
-    if (i < charArray.length) {
-      charArray[i].classList.add('current-char');
     }
   }
 }
 
-$html.addEventListener('keypress', handleKeyPress);
-console.log(charArray);
+document.addEventListener('keypress', handleKeyPress);
