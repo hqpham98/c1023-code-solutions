fcfe1b8b46377a85563bec22d776d066e9182166
Henry Pham <hqpham98@gmail.com>
2023-11-22 18:48:57 +0000

    exercise completed

---
+++ javascript-ajax/users.js
@@ -0,0 +1,18 @@
+const $userList = document.querySelector('#user-list');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  console.log('status:', xhr.status);
+  console.log('response:', xhr.response);
+
+  for (const user of xhr.response) {
+    const li = document.createElement('li');
+    li.textContent = user.name;
+    $userList.appendChild(li);
+  }
+});
+
+console.log(xhr);
+xhr.send();
