773881589e179b70510cecac4e5865ba78b2bf14
Henry Pham <hqpham98@gmail.com>
2023-12-06 22:57:39 +0000

    Completed exercise

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,16 @@
+const fullname: string = 'Henry Pham';
+const isCool: boolean = true;
+const totalPets: number = 1;
+const unknown: null = null;
+const never: undefined = undefined;
+
+console.log('value of fullname:', fullname);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('value of unknown:', unknown);
+console.log('value of never:', never);
+console.log('typeof fullname:', typeof fullname);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+console.log('typeof unknown:', typeof unknown);
+console.log('typeof never:', typeof never);
