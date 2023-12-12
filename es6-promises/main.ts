8eee6631be90cc5dfb1a7fd267918643f9bb95a0
Henry Pham <hqpham98@gmail.com>
2023-12-12 23:06:47 +0000

    Completed exercise

--- es6-promises/main.ts
+++ es6-promises/main.ts
@@ -1,1 +1,9 @@
 import { takeAChance } from './take-a-chance.js';
+
+takeAChance('Henry')
+  .then((pass) => {
+    console.log(pass);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
