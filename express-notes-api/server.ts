e635d1f2d4dd0704d2a55f4e1b4c172839a51307
Henry Pham <hqpham98@gmail.com>
2023-12-18 08:38:21 +0000

    Completed exercise

--- express-notes-api/server.ts
+++ express-notes-api/server.ts
@@ -1,53 +1,73 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import fs from 'fs/promises';
 
 const app = express();
 
 type Note = {
-  id: number;
-  content: string;
+  nextId: number;
+  notes: Record<string, number>;
 };
 
-let notes: Record<string, Note> = {};
-
-try {
-  updateNotes();
-} catch (e) {
-  console.log(e);
-}
+let notes: Note;
 
 async function updateNotes(): Promise<void> {
   const d = await fs.readFile('data.json', 'utf-8');
   notes = JSON.parse(d);
 }
-async function read(req, res, next): Promise<void> {
+
+async function read(req: Request, res: Response): Promise<void> {
   try {
-    updateNotes();
     res.json(notes);
-    next();
   } catch (e) {
     console.log(e);
   }
 }
 
-async function write(req, res, next): Promise<void> {
+async function write(req: Request, res: Response): Promise<void> {
   try {
+    if (
+      !(
+        Object.keys(req.body).length === 1 &&
+        'content' in req.body &&
+        typeof req.body.content === 'string'
+      )
+    ) {
+      res.status(404).send('Invalid arguments');
+      return;
+    }
+    const newNote = { id: notes.nextId, ...req.body };
+    notes.notes[notes.nextId] = newNote;
+    notes.nextId++;
+    await fs.writeFile('data.json', JSON.stringify(notes, null, 2), 'utf-8');
     updateNotes();
+    res.status(201).json(newNote);
   } catch (e) {
     console.log(e);
   }
 }
 
-async function deleteNote(req, res, next): Promise<void> {
+async function deleteNote(req: Request, res: Response): Promise<void> {
   try {
-    console.log(req);
+    if (!(req.params.id in notes.notes)) {
+      res.status(404).send('Invalid ID');
+      return;
+    }
+    delete notes.notes[req.params.id];
+    await fs.writeFile('data.json', JSON.stringify(notes, null, 2), 'utf-8');
+    updateNotes();
+    res.sendStatus(204);
   } catch (e) {
     console.log(e);
   }
 }
 
+try {
+  await updateNotes();
+} catch (e) {
+  console.log(e);
+}
 app.get('/api/notes/', read);
-// app.use(express.json());
-// app.post('/api/notes/:id', write);
+app.use(express.json());
+app.post('/api/notes/', write);
 app.delete('/api/notes/:id', deleteNote);
 app.listen(8080, () => console.log('Listening on port 8080'));
