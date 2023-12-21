5289d930d171072992ace54bdc2f9b69d71a72bc
Henry Pham <hqpham98@gmail.com>
2023-12-21 18:10:32 +0000

    Completed exercise

---
+++ react-props-and-expressions/my-app/src/CustomButton.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  text: string;
+  color: string;
+};
+
+export function CustomButton({ text, color }: Props) {
+  return (
+    <>
+      <button
+        style={{
+          backgroundColor: color,
+        }}>
+        {text}
+      </button>
+    </>
+  );
+}
