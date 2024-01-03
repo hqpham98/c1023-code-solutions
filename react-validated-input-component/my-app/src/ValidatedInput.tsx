d52efb7026d92623517aadea899490b39400a10f
Henry Pham <hqpham98@gmail.com>
2024-01-03 23:24:57 +0000

    Completed exercise

--- react-validated-input-component/my-app/src/ValidatedInput.tsx
+++ react-validated-input-component/my-app/src/ValidatedInput.tsx
@@ -1,59 +1,31 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import { FcCheckmark, FcCancel } from 'react-icons/fc';
 
 export function ValidatedInput() {
-  const [pass1, setPass1] = useState<string>('');
-  const [pass2, setPass2] = useState<string>('');
-  const [pass3, setPass3] = useState<string>('');
+  const [pass, setPass] = useState<string>('');
+  const [valid, setValid] = useState<boolean>(false);
 
-  function handleInput(e: ChangeEvent<HTMLInputElement>) {
-    if (e.currentTarget.name == 'pass1') {
-      setPass1(e.currentTarget.value);
-    }
-    if (e.currentTarget.name == 'pass2') {
-      setPass2(e.currentTarget.value);
-    }
-    if (e.currentTarget.name == 'pass3') {
-      setPass3(e.currentTarget.value);
-    }
-  }
-
-  function PasswordWarning({ pass }: Record<string, string>) {
+  function PasswordWarning() {
     if (!pass) {
+      setValid(false);
       return <p>A password is required.</p>;
     }
     if (pass.length < 8) {
+      setValid(false);
       return <p>Your password is too short.</p>;
     }
-  }
-
-  function ValidPass({ pass }: Record<string, string>) {
-    if (!pass || pass.length < 8) {
-      return <FcCancel />;
-    }
-    return <FcCheckmark />;
+    setValid(true);
   }
 
   return (
-    <>
-      <label>
-        <p>Password</p>
-        <input name="pass1" onChange={handleInput}></input>
-        <ValidPass pass={pass1} />
-        <PasswordWarning pass={pass1} />
-      </label>
-      <label>
-        <p>Password</p>
-        <input name="pass2" onChange={handleInput}></input>
-        <ValidPass pass={pass2} />
-        <PasswordWarning pass={pass2} />
-      </label>
-      <label>
-        <p>Password</p>
-        <input name="pass3" onChange={handleInput}></input>
-        <ValidPass pass={pass3} />
-        <PasswordWarning pass={pass3} />
-      </label>
-    </>
+    <label>
+      <p>Password</p>
+      <input
+        name="password"
+        onChange={(e) => setPass(e.currentTarget.value)}
+        value={pass}></input>
+      {valid ? <FcCheckmark /> : <FcCancel />}
+      <PasswordWarning />
+    </label>
   );
 }
