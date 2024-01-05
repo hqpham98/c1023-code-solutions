58c4f488f72e2ba5725ca7acb1946715a87e205c
Henry Pham <hqpham98@gmail.com>
2024-01-05 23:27:23 +0000

    Completed  exercise

--- react-app-drawer-component/my-app/src/App.tsx
+++ react-app-drawer-component/my-app/src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
+import logo from '../../hylian-emblem.svg';
+import { useState } from 'react';
 import { AppDrawer } from './AppDrawer';
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState('');
+
   const menuItems = [
     'The Legend of Zelda',
     'A Link to the Past',
@@ -9,7 +13,22 @@ function App() {
     'The Wind Waker',
     'Breath of the Wild',
   ];
-  return <AppDrawer items={menuItems} />;
+
+  return (
+    <>
+      <div>
+        <AppDrawer
+          heading="Choose a Game"
+          items={menuItems}
+          onSelect={setSelectedItem}
+        />
+      </div>
+      <div className="row">{selectedItem}</div>
+      <div className="row">
+        <img src={logo} />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- react-app-drawer-component/my-app/src/AppDrawer.tsx
+++ react-app-drawer-component/my-app/src/AppDrawer.tsx
@@ -1,21 +1,52 @@
 import './AppDrawer.css';
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
 
-type MenuProps = {
+type DrawerProps = {
+  heading: string;
   items: string[];
+  onSelect: (x: string) => void;
 };
 
 type ItemProps = {
   item: string;
+  setOpen: (x: boolean) => void;
+  onSelect: (x: string) => void;
 };
 
-export function AppDrawer({ items }: MenuProps) {
-  return <MenuContainer items={items} />;
+export function AppDrawer({ heading, items, onSelect }: DrawerProps) {
+  const [open, setOpen] = useState(false);
+
+  const menuItems = [];
+  for (let i = 0; i < items.length; i++) {
+    menuItems.push(
+      <MenuItem key={i} item={items[i]} setOpen={setOpen} onSelect={onSelect} />
+    );
+  }
+  return (
+    <div>
+      <div
+        className={open ? 'overlay' : 'hidden'}
+        onClick={() => setOpen(false)}></div>
+      <div className={open ? 'app-drawer' : 'app-drawer close'}>
+        <h1>{heading}</h1>
+        {menuItems}
+      </div>
+      <div>
+        <FaBars className="menu-button" onClick={() => setOpen(true)} />
+      </div>
+    </div>
+  );
 }
 
-function MenuContainer({ items }: MenuProps) {
-  return <MenuButton item={items[0]} />;
-}
-
-function MenuButton({ item }: ItemProps) {
-  return <button>{item}</button>;
+function MenuItem({ item, setOpen, onSelect }: ItemProps) {
+  function handleClick() {
+    setOpen(false);
+    onSelect(item);
+  }
+  return (
+    <div className="menu-item" onClick={handleClick}>
+      <button>{item}</button>
+    </div>
+  );
 }
