48944cdce9b34a6cf085bc83feda48e7d33b44f7
Henry Pham <hqpham98@gmail.com>
2024-01-10 19:45:05 +0000

    Revised error handling

--- fetch/main.js
+++ fetch/main.js
@@ -1,11 +1,10 @@
 async function getStuff() {
   try {
     const result = await fetch('https://jsonplaceholder.typicode.com/users/');
-    if (result.ok) {
-      console.log('user objects:', await result.json());
-    } else {
-      console.log(`Error, Response Code: ${result.status}`);
+    if (!result.ok) {
+      throw Error(`Error, Response Code: ${result.status}`);
     }
+    console.log('user objects:', await result.json());
   } catch (err) {
     console.log(err);
   }
